6d1b7da272f282aa09c829efbc1ab11ed2e6baaf
Maxim Azarpv <max6808025@gmail.com>
2025-11-06 04:07:34 +0100

    Fix errors

--- frontend/src/hooks/usePins.js
+++ frontend/src/hooks/usePins.js
@@ -4,7 +4,7 @@ import { getPublicPins } from '../services/pinService';
 export default function usePins() {
     const { data, loading, error } = useAsync(
         async () => {
-            const response = await getPublicPins();
+            const response = await getPublicPins(1, 55);
             if (Array.isArray(response && response.data && response.data.items)) return response.data.items;
             if (Array.isArray(response && response.data)) return response.data;
             if (Array.isArray(response)) return response;
